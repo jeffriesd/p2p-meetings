a960bf02dadfd1967c5a9fb9a99d205a935b7944
danjeffries96 <danjeffries96@github.com>
2020-11-21 17:09:12 -0500

    first commit

--- p2p-client.py
+++ p2p-client.py
@@ -1,4 +1,4 @@
-from sockets import *
+from socket import *
 
 
 
@@ -8,40 +8,97 @@ from sockets import *
 P2P_PORT = 40
 
 
+# maximum outgoing connections 
+MAX_OUTGOING = 5
 
-class PeerNode:
+
+class HostNode:
     """
     """
 
-    def __init__(self):
+    def __init__(self, name):
+        self.room_name = name
+
         # maintain list of peer ips
         self.peers = []
 
-        # maintain dictionary of tcp connections
-        # keyed on ip addr
+        # maintain dictionary of tcp connections 
+        # (socket objects) keyed on ip addr
         self.connections = {}
 
-    def connect_and_listen(self, addr):
+    def wait_for_connections(self):
         """
-        Connect with peer and listen for incoming data
+        Wait for incoming tcp connection requests, 
+        then send some test data when they connect.
         """
-        # create new tcp connection with peer
-        client_socket = socket(AF_INET, SOCK_STREAM)
-        client_socket.connect((addr, P2P_PORT))
+        # TODO put this in its own thread
+        
+        server_socket = socket(AF_INET, SOCK_STREAM)
+        server_socket.bind(('', P2P_PORT))
+        server_socket.listen(MAX_OUTGOING)
+
+        while True:
+            connection_socket, addr = server_socket.accept()
+            test_string = "You are connected to host %s!" % self.room_name
+            connection_socket.send(test_string.encode())
+
+            # add this connection to the list
+            self.connections[addr] = connection_socket
+
+
+            # TODO just for debugging
+            break
+
+    def broadcast_message(self, msg_str):
+        """
+        Given a message as a string, send it to every peer. 
+        """
+        msg_bytes = msg_str.encode()
+        for peer_addr in self.connections:
+            # send encoded message
+            self.connections[peer_addr].send(msg_bytes)
+
+
+
+class AudienceNode:
+
+    def __init__(self, host_addr):
+        """
+        Connect to host and save reference to the socket object.
+        """
+        self.host_addr = host_addr
+        # create new tcp connection with hosting peer
+        self.client_socket = socket(AF_INET, SOCK_STREAM)
+
+        # TODO ERROR HANDLING
+        try:
+            self.client_socket.connect((host_addr, P2P_PORT))
+            print("Connected successfully")
+            
+            # listen for messages from host 
+            self.listen_to_host()
+        except:
+            self.client_socket.close()
+            print("Connection failed")
+
+
+        
+    def listen_to_host(self):
+        """
+        Connect with host and listen for incoming data
+        """
 
         # TODO create a new thread to listen 
 
         # listen for data on tcp connection 
-        response_bytes = bytearray()
         while True:
-            response = client_socket.recv(1024)
+            response = self.client_socket.recv(1024)
+
+            # TODO add error handling in case connection
+            # is closed by host 
             if not response:
                 break
-            response_bytes += response
-
-
-
-        
+            print("New message: %s" % response.decode())
 
 
     

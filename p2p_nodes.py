d3039ad93ce89fc24aa618ef106964e2baee19f3
danjeffries96 <danjeffries96@github.com>
2020-12-09 23:01:25 -0500

    change ports

--- p2p_nodes.py
+++ p2p_nodes.py
@@ -171,8 +171,8 @@ class HostNode:
         Safely get username by checking first if 
         peer is still connected.
         """
-        print("get username for ", addr_port)
         if addr_port in self.peers:
+            print("get username for ", addr_port, " = ", self.peers[addr_port].username)
             return self.peers[addr_port].username
 
         return self.unknown_peer_error(addr_port)
@@ -364,7 +364,7 @@ class MeshAudienceNode(HostNode):
                 self.add_new_peer(conn_socket, addr_port)
 
                 # also broadcast username to these peers
-                print("my username is ", self.username)
+                print("my username is ", self.username) #DEBUG
                 send_socket_message(conn_socket, RegisterUsername(self.username))
 
                 # send connection message

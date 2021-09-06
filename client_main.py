5975b0d18164e6246d5a8a4e970b8c6712603dfd
danjeffries96 <danjeffries96@github.com>
2021-09-06 14:39:36 -0400

    add logging for server, client

--- client_main.py
+++ client_main.py
@@ -1,6 +1,23 @@
-
-
+import logging
+import sys
 from p2p_meetings.client import * 
 
 if __name__ == "__main__":
+    logging.basicConfig(filename="client.log", level=logging.DEBUG)
+
+    # create new log stream handler to print logs to stdout
+    # in addition to log file 
+    handler = logging.StreamHandler(sys.stdout)
+    # only print INFO for client 
+    handler.setLevel(logging.INFO)
+    logging.getLogger().addHandler(handler)
+
+    logging.debug("""\n
+---------------------------------
+------ Starting new client ------
+---------------------------------
+    """)
+
+
+
     c = Client()
--- server_main.py
+++ server_main.py
@@ -1,19 +1,20 @@
-
-
 from p2p_meetings.central_server import * 
+import sys
+import logging
 
 if __name__ == "__main__":
-    import logging
     logging.basicConfig(filename="server.log", level=logging.DEBUG)
+
+    # create new log stream handler to print logs to stdout
+    # in addition to log file 
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    logging.getLogger().addHandler(handler)
+
     logging.info("""\n
 -----------------------------
 ------ Starting server ------
 -----------------------------
     """)
 
-    # TODO print output of log file when running main 
-
-    print("Starting server...")
     server = Server()
-
-
